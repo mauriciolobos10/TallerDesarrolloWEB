2df82ff07a885bf7cecd6c1bc3d0bfb00d0498d8
mauricio <mauri.loboss@gmail.com>
2022-10-12 06:53:55 -0300

    react

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import { Card, CardContent } from "@mui/material"
+
+import React, { Component }  from 'react';
+
+const Home = () => {
+    return (
+        <Card>
+            <CardContent>
+                Hola2
+            </CardContent>        
+        </Card>
+    );
+}
+
+export default Home;
