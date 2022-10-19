928bf32016756a69d6ec781ecbdc6ef7f05e7e15
mauricio <mauri.loboss@gmail.com>
2022-10-19 09:25:53 -0300

    web 3

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,6 +15,18 @@ const Home = () => {
     const [errors, setErrors] = useState(false);
     const [listadoAux, setListadoAux] = useState([]);
     const [count, setCount] = useState(1)
+    const [listadoSelected, setListadoSelected] = useState([]);
+
+
+    const arreglar = () =>{
+        listado.sort((a,b) => {
+        return a.name > b.name});
+    }
+
+    const arreglarPrincipio =(listado) => {
+        let copiaListado = listado.sort((a,b) => a.name > b.name ? 1 : -1);
+        return copiaListado;
+    }
 
 
     function sumar(){
@@ -26,12 +38,17 @@ const Home = () => {
         axios.get("https://pokeapi.co/api/v2/pokemon?limit=151").then(
             (response) => {
                 console.log(response.data.results);
-                setListado(response.data.results);
+                let datos = response.data.results;
+                let ordenado = arreglarPrincipio(datos)
+
+                setListado(ordenado);
             },
             (error) => {
                 console.log(error);
             }
         );
+        
+
     };
     const handleInputChange = (event)=> {
         setFinder(event.target.value);
@@ -41,11 +58,45 @@ const Home = () => {
             let result = listado.filter((item) => 
             item.name.toString().includes(finder.toString().trim())
             );
-            setListadoAux(result);
+
+            if(result.lenght !== 0){
+                errors && setErrors(false);
+                setListadoAux(result);
+            }else{
+                setListadoAux([]);
+                setErrors(true);
+            }
+            //setListadoAux(result);
         }
         console.log(finder);
     }, [finder]);
     let estilo = {backgroundColor: 'red'}
+
+    const stack = (itemExterno) => {
+        setListadoSelected((listadoSelected) => [...listadoSelected, itemExterno]);
+
+        // let aux = [...listadoSelected, itemExterno];
+        // setListadoSelected((listadoSelected) => arreglarPrincipio(aux));
+
+        let result = listado.filter((item) => item.name !== itemExterno.name);
+        setListado(result);
+
+        let resultClon = listadoAux.filter((item) => item.name !== itemExterno.name);
+        setListadoAux(resultClon);
+    }
+
+    const stack2 = (itemExterno) => {
+        setListado((listado) => [...listado, itemExterno]);
+
+        let result = listadoSelected.filter((item) => item.name !== itemExterno.name);
+        setListadoSelected(result);
+        arreglar();
+
+        // let resultClon = listadoAux.filter((item) => item.name !== itemExterno.name);
+        // setListadoAux(resultClon);
+    }
+
+    
     
 
 
@@ -67,17 +118,33 @@ const Home = () => {
                         
                 </TextField>
                 <Grid container spacing={1}>
-                    <Grid item md={6}>
+                    <Grid item md={4}>
                         {listado.map((element, index) => (
-                            <Poke dato= {element.name} key={index}></Poke>
+                            <Poke pokemon= {element} dato= {element.name} key={index}></Poke>
                         ))}
                     </Grid>
 
-                    <Grid item md={6}>
+                    <Grid item md={4}>
                         
                         {finder && 
                         listadoAux.map((element, index) => (
-                            <Poke dato= {element.name} key={index} estilo={estilo}></Poke>
+                            <Poke 
+                            pokemon= {element}
+                            dato= {element.name} 
+                            key={index} 
+                            estilo={estilo}
+                            funcion={stack}
+                            ></Poke>
+                        ))}
+                    </Grid>
+                    <Grid item md={4}>
+                        {listadoSelected.map((element, index) => (
+                            <Poke 
+                            pokemon= {element} 
+                            dato= {element.name} 
+                            key={index}
+                            funcion={stack2}
+                            ></Poke>
                         ))}
                     </Grid>
 
